a729acd1a5e2a35b4292df5aa7a7972a3fba9ba3
Sandip Roy <sandiproyofficial29@gmail.com>
2026-01-30 17:54:02 +0530

    Create registerUser endpoint & improve some code and solve some bug.

--- Daily-Notes-Backend/src/app.js
+++ Daily-Notes-Backend/src/app.js
@@ -8,9 +8,9 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
+app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(express.json({limit: "16kb"}));
 
 // routes import
 import userRouter from './routes/user.routes.js'
--- Daily-Notes-Backend/src/controllers/user.controller.js
+++ Daily-Notes-Backend/src/controllers/user.controller.js
@@ -1,9 +1,43 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message: "ok",
+
+    // Take information
+    const {username, email, password} = req.body || {};
+    
+    // check imformations
+    if([username,email,password].some((field) => !field || field.trim() === "")){
+        throw new ApiError(400,"All fields are required");
+    }
+    
+    // check if user already existed or not
+    const existedUser = await User.findOne({
+        $or: [{ username },{ email }]
     })
+    
+    if(existedUser) throw new ApiError(409,"User already existed");
+    
+    // create user object and enter in DB
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        password,
+    })
+    
+    // remove password and refreshtoken from response
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    // check for user creation
+    if(!createdUser) throw new ApiError(500,"Something went wrong while registering the user");
+    
+    // res return
+    return res.status(201).json(new ApiResponse(200,createdUser,"User registered successfully"));
 })
+console.log(registerUser);
 
 export { registerUser};
