a74be92d50ee07c2e7e8def49e3b0799ae4d16dc
Sandip Roy <sandiproyofficial29@gmail.com>
2026-01-30 10:28:53 +0530

    Create user registration route for testing router flow & add json middleware.

--- Daily-Notes-Backend/src/app.js
+++ Daily-Notes-Backend/src/app.js
@@ -10,5 +10,12 @@ app.use(cors({
 }));
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use(express.json({limit: "16kb"}));
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
