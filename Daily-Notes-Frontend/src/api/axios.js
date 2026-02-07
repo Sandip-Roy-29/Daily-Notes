ee0dc9c8a58562f7d89f9c538feb18fa296a453b
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-07 14:19:03 +0530

    Refresh tokens to prevent uncessary login

--- Daily-Notes-Frontend/src/api/axios.js
+++ Daily-Notes-Frontend/src/api/axios.js
@@ -8,11 +8,38 @@ const api = axios.create(
 )
 
 api.interceptors.response.use(
+
+    // Success handler
     res => res,
-    err => {
-        if(err.response?.status === 401){
-            window.location.href = "/login";
+
+    // Error handler(4xx/5xx)
+    async err => {
+
+        // Axios attaches the original request config to the error.
+        const originalRequest = err.config;
+
+        // Check for expired access token
+        if(err.response?.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true;
+
+            try {
+
+                // Try refreshing the token
+                await api.post("/users/refresh-token");
+
+                // Retry the original request
+                return api(originalRequest);
+
+            } catch (refreshError) {
+
+                // Redirect to login
+                window.location.href = "/login";
+
+                // Return the error
+                return Promise.reject(refreshError);
+            }
         }
+        // Return the error
         return Promise.reject(err);
     }
 )
