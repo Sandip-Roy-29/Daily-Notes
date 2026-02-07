b22ed7ea5c9369d14f915182f1bc5c717fa109fa
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-07 11:58:30 +0530

    Use axios interceptors to handle 401 error

--- Daily-Notes-Frontend/src/api/axios.js
+++ Daily-Notes-Frontend/src/api/axios.js
@@ -7,4 +7,14 @@ const api = axios.create(
     }
 )
 
+api.interceptors.response.use(
+    res => res,
+    err => {
+        if(err.response?.status === 401){
+            window.location.href = "/login";
+        }
+        return Promise.reject(err);
+    }
+)
+
 export default api;
