7d49aa2c85a86940bb21c55fae35e083c8e20bd9
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-07 14:20:08 +0530

    Create all the routes of user

--- Daily-Notes-Frontend/src/App.jsx
+++ Daily-Notes-Frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Note from "./pages/Note";
-import LogoutButton from "./pages/LogoutButton";
+import Register from "./pages/Register";
+import ChangePassword from "./pages/ChangePassword";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
 
   return (
     <Routes>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/notes" element={<Note/>}/>
+      <Route path="/register" element={<Register/>}/>
+
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/notes" element={<Note/>}/>
+        <Route path="/change-password" element={<ChangePassword/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/edit-profile" element={<EditProfile/>}/>
+        </Route>
     </Routes>
   )
 }
