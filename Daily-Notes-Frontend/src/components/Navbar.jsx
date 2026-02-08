58361eac85e194fc2b20cc5e446c8cf65e5e08a4
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-08 10:06:05 +0530

    Create Navbar and Home page

---
+++ Daily-Notes-Frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+function Navbar(){
+    const { user, logout } = useAuth();
+
+    return(
+        <nav style={styles.nav}>
+            <h3>DailyNotes</h3>
+
+            <div style={styles.links}>
+                <Link to="/">Home</Link>
+
+                {user && <Link to="/notes">Notes</Link>}
+
+                {!user && (
+                    <>
+                        <Link to={"/login"}>Login</Link>
+                        <Link to={"/register"}>Register</Link>
+                    </>
+                )}
+
+                {user && (
+                    <>
+                        <Link to={"/profile"}>Profile</Link>
+                        <button onClick={logout}>Logout</button>
+                    </>
+                )}
+
+            </div>
+        </nav>
+    )
+}
+
+const styles = {
+    nav:{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px 20px",
+        background: "#111",
+        color: "white"
+    },
+    links:{
+        display: "flex",
+        gap: "15px",
+        alignItems: "center"
+    },
+};
+
+export default Navbar;
