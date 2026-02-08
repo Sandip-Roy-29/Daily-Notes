1ab74ca8360b2f13adacfcea63c4af9d8196111a
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-08 10:01:50 +0530

    Create AuthContext for centralize fetching and wrap App with AuthProvider

---
+++ Daily-Notes-Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import { createContext,useEffect, useState } from "react";
+import api from "../api/axios";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await api.get("/users/current-user");
+                setUser(res.data.data);
+            } catch {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    const login = async (identifier, password) => {
+        await api.post("/users/login", { identifier, password });
+        const res = await api.get("/users/current-user");
+        setUser(res.data.data);
+    };
+
+    const logout = async () => {
+        await api.post("/users/logout");
+        setUser(null);
+        window.location.href = "/login";
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                isAuth: !!user,
+                loading,
+                login,
+                logout,
+                setUser,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
