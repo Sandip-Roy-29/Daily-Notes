bb0421c0c5ef6c305ced99e8449b6803ab3dc9f6
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-15 19:01:58 +0530

    1. Divide Loading into ActionLoading an AuthLoading 2. wrap every function with try/catch 3. Create register function 4. Create change password and update info function

--- Daily-Notes-Frontend/src/context/AuthProvider.jsx
+++ Daily-Notes-Frontend/src/context/AuthProvider.jsx
@@ -1,47 +1,132 @@
 import { AuthContext } from "./AuthContext";
 import { useState, useEffect } from "react";
-import api from "../api/axios";
+import { changePassword, fetchUser, loginUser, logoutUser, registerUser, updateUserInfo } from "../api/auth.api";
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [authLoading, setAuthLoading] = useState(true);
+    const [actionLoading, setActionLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchUser = async () => {
+        const fetchCurrentUser = async () => {
+            setActionLoading(true);
             try {
-                const res = await api.get("/users/current-user");
+                const res = await fetchUser();
                 setUser(res.data.data);
             } catch(err) {
-                if(err.response?.status === 401) setUser(null);
+                setError(err.response?.data?.message || "Failed to fetch user");
+                setUser(null);
             } finally {
-                setLoading(false);
+                setAuthLoading(false);
             }
         };
 
-        fetchUser();
+        fetchCurrentUser();
     }, []);
 
+    const register = async (data) => {
+        setActionLoading(true);
+        setError(null);
+
+        try {
+            const res = await registerUser(data);
+            setUser(res.data);
+            return true;
+        } catch (err) {
+            setError(err.response?.data?.message || "Registration failed");
+            return false;
+        } finally {
+            setActionLoading(false);
+        }
+    }
+
     const login = async (identifier, password) => {
-        await api.post("/users/login", { identifier, password });
-        const res = await api.get("/users/current-user");
-        setUser(res.data.data);
+        setActionLoading(true);
+        setError(null);
+
+        try {
+            const res = await loginUser({ identifier, password });
+            setUser(res.data);
+            return true;
+        } catch (err) {
+            setError(err.response?.data?.message || "Login failed");
+            return false;
+        } finally {
+            setActionLoading(false);
+        }
     };
 
+    const refreshUser = async () => {
+        try {
+            const res = await fetchUser();
+            setUser(res.data);
+        } catch {
+            setUser(null);
+        }
+    }
+
     const logout = async () => {
-        await api.post("/users/logout");
-        setUser(null);
-        window.location.href = "/login";
+        try {
+            await logoutUser();
+            setUser(null);
+        } catch (err) {
+            setError(err.response?.data?.message || "Logout failed");
+        } finally {
+            setActionLoading(false);
+        }
     };
+
+    const changeUserPassword = async (currentPassword, newPassword, confirmPassword) => {
+        setActionLoading(true);
+        setError(null);
+
+        try {
+            await changePassword({
+                currentPassword,
+                newPassword,
+                confirmPassword
+            })
+            return true;
+        } catch (err) {
+            setError(err.response?.data?.message || "Password change failed");
+            return false;
+        } finally {
+            setActionLoading(false);
+        }
+    }
+
+    const UpdateInfo = async (username) => {
+        setActionLoading(true);
+        setError(null);
+
+        try {
+            const res = await updateUserInfo({username});
+            setUser(res.data);
+            return true;
+        } catch (err) {
+            setError(err.response?.data?.message || "Username change failed");
+            return false;
+        } finally {
+            setActionLoading(false);
+        }
+    }
 
     return (
         <AuthContext.Provider
             value={{
                 user,
                 isAuth: !!user,
-                loading,
+                authLoading,
+                actionLoading,
+                error,
                 login,
                 logout,
+                changeUserPassword,
                 setUser,
+                refreshUser,
+                UpdateInfo,
+                register
             }}
         >
             {children}
