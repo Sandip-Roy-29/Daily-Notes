2c4a028a53dbc0d5e90db000a33068a7b108cd3f
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-23 22:02:02 +0530

    Refactor MongoDB connection logic to use environment-specific URIs and improve connection options

--- Daily-Notes-Backend/src/db/index.js
+++ Daily-Notes-Backend/src/db/index.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+
+let uri;
+
+if(process.env.NODE_ENV === "test") uri = process.env.MONGODB_URI_TEST;
+else if(process.env.NODE_ENV === "development") uri = process.env.MONGODB_URI_DEV;
+else uri = process.env.MONGODB_URI_PROD;
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(
+            uri,
+            {
+                maxPoolSize: parseInt(process.env.MONGODB_MAX_POOL_SIZE || 10),
+                minPoolSize: parseInt(process.env.MONGODB_MIN_POOL_SIZE || 5),
+                socketTimeoutMS: 30000, // 30 seconds
+                serverSelectionTimeoutMS: 30000, // 30 seconds
+                heartbeatFrequencyMS: 10000, // 10 seconds
+            }
+        );
         console.log(`MongoDB connected !! DB host ${connectionInstance.connection.host}`);        
     } catch (error) {
         console.log("MongoDB connection failed ", error);
