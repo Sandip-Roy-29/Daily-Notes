04a65b364f70a2f74cc18decb032d2681e1dcc4b
Sandip Roy <sandiproyofficial29@gmail.com>
2026-02-23 22:11:50 +0530

    Add sendMail utility for handling email notifications

---
+++ Daily-Notes-Backend/src/utils/sendMail.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+export const sendMail = async({name, email, type, subject, message}) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      }
+    });
+  
+    const result = await transporter.sendMail({
+      from: `"Daily Notes Contact" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_USER,
+      subject: `[${type.toUpperCase()}] ${subject}`,
+      replyTo: email,
+      text:`
+      Message Type: ${type}
+      From: ${name}
+      User Email: ${email}
+      Message: ${message}
+      `
+    })
+
+    console.log("Email sent successfully:", result.messageId);
+    
+  } catch (error) {
+    console.error("Error sending email:", error.message);
+    throw error;
+  }
+}
