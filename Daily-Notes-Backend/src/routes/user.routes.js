94f6305343ca87bb5f01f12fd8a2cbefc293d50e
Sandip Roy <sandiproyofficial29@gmail.com>
2026-03-07 10:54:22 +0530

    Apply rate limit middleware in routes

--- Daily-Notes-Backend/src/routes/user.routes.js
+++ Daily-Notes-Backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, deleteAccount } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { authLimiter } from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
@@ -12,13 +13,13 @@ router.get("/health", (_, res) => {
     });
 });
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+router.route("/register").post(authLimiter, registerUser);
+router.route("/login").post(authLimiter, loginUser);
 router.route("/logout").post(logoutUser);
 
 // secured routes
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/change-password").post(verifyJWT, authLimiter, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-credentials").put(verifyJWT, updateAccountDetails);
 router.route("/delete").delete(verifyJWT, deleteAccount);
