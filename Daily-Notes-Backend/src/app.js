a698ee90830b943d4c4161667d77373892ea9966
Sandip Roy <sandiproyofficial29@gmail.com>
2026-03-07 10:56:08 +0530

    Import helmet and morgon middleware and apply

--- Daily-Notes-Backend/src/app.js
+++ Daily-Notes-Backend/src/app.js
@@ -2,8 +2,29 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error.middleware.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import { limiter } from "../src/middlewares/rateLimiter.middleware.js";
 
 const app = express();
+
+// Security middlewares
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            scriptSrc: ["'self'"],
+            imgSrc: ["'self'", "data:", "https:"],
+        }
+    }
+}));
+
+const morganFormat = ":method :url :status :response-time ms";
+if(process.env.NODE_ENV !== "test"){
+    app.use(limiter);
+    app.use(morgan(morganFormat));
+}
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -16,10 +37,12 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.routes.js"
 import notesRouter from "./routes/notes.routes.js"
+import contactRouter from "./routes/contact.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/notes", notesRouter);
+app.use("/api/v1/contact", contactRouter);
 
 // error middleware
 app.use(errorHandler)
